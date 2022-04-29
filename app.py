b7a27fa7fba9763872a0781a2f59de0eecd99c56
AkshitChugh787 <akshit.chugh2001@gmail.com>
2022-04-29 13:10:24 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from contextlib import redirect_stderr
 from email.policy import default
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -18,19 +19,39 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route("/")
+@app.route("/", methods = ['GET', "POST"])
 def hello_world():
-    todo = Todo(title = "First Todo", desc = "go to sleep")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title = title, desc = desc)
+        db.session.add(todo)
+        db.session.commit()
+        
     allTodo = Todo.query.all()
     return render_template('index.html', allTodo=allTodo)
 
-@app.route("/show")
-def show():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return ''
+@app.route("/update/<int:sno>", methods = ['GET', "POST"])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
